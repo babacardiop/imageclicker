73744a072bd353cd489cbe8dfa8f614b5f70cd02
El Hadji Babacar Diop <bdiop@edilex.com>
2018-12-02 15:22:23 -0500

    split register and login

--- ImageClicker/Controllers/AccountsController.cs
+++ ImageClicker/Controllers/AccountsController.cs
@@ -1,84 +1,48 @@
-﻿using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using ImageClicker.Auth;
+﻿using System.Threading.Tasks;
+using AutoMapper;
 using ImageClicker.Helpers;
-using ImageClicker.Models;
 using ImageClicker.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using Repositories.Entities;
 
 namespace ImageClicker.Controllers
 {
     [Route("api/[controller]")]
-    public class AuthController : Controller
+    public class AccountsController : Controller
     {
+        private readonly ApplicationDbContext _appDbContext;
         private readonly UserManager<AppUser> _userManager;
-        private readonly IJwtFactory _jwtFactory;
-        private readonly JsonSerializerSettings _serializerSettings;
-        private readonly JwtIssuerOptions _jwtOptions;
+        private readonly IMapper _mapper;
 
-        public AuthController(UserManager<AppUser> userManager, IJwtFactory jwtFactory, IOptions<JwtIssuerOptions> jwtOptions)
+        public AccountsController(UserManager<AppUser> userManager, IMapper mapper, ApplicationDbContext appDbContext)
         {
             _userManager = userManager;
-            _jwtFactory = jwtFactory;
-            _jwtOptions = jwtOptions.Value;
-
-            _serializerSettings = new JsonSerializerSettings
-            {
-                Formatting = Formatting.Indented
-            };
+            _mapper = mapper;
+            _appDbContext = appDbContext;
         }
 
-        // POST api/auth/login
-        [HttpPost("login")]
-        public async Task<IActionResult> Post([FromBody]CredentialsViewModel credentials)
+        // POST api/accounts
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]RegistrationViewModel model)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var identity = await GetClaimsIdentity(credentials.UserName, credentials.Password);
-            if (identity == null)
-            {
-                return BadRequest(Errors.AddErrorToModelState("login_failure", "Invalid username or password.", ModelState));
-            }
+            var userIdentity = _mapper.Map<AppUser>(model);
 
-            // Serialize and return the response
-            var response = new
-            {
-                id = identity.Claims.Single(c => c.Type == "id").Value,
-                auth_token = await _jwtFactory.GenerateEncodedToken(credentials.UserName, identity),
-                expires_in = (int)_jwtOptions.ValidFor.TotalSeconds
-            };
+            var result = await _userManager.CreateAsync(userIdentity, model.Password);
 
-            var json = JsonConvert.SerializeObject(response, _serializerSettings);
-            return new OkObjectResult(json);
-        }
+            if (!result.Succeeded) return new BadRequestObjectResult(Errors.AddErrorsToModelState(result, ModelState));
 
-        private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
-        {
-            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password))
-            {
-                // get the user to verifty
-                var userToVerify = await _userManager.FindByNameAsync(userName);
+            await _appDbContext.Seeders.AddAsync(new ImageSeeder { IdentityId = userIdentity.Id, Location = model.Location });
+            await _appDbContext.Clickers.AddAsync(new Repositories.Entities.ImageClicker { IdentityId = userIdentity.Id, Location = model.Location });
 
-                if (userToVerify != null)
-                {
-                    // check the credentials  
-                    if (await _userManager.CheckPasswordAsync(userToVerify, password))
-                    {
-                        return await Task.FromResult(_jwtFactory.GenerateClaimsIdentity(userName, userToVerify.Id));
-                    }
-                }
-            }
+            await _appDbContext.SaveChangesAsync();
 
-            // Credentials are invalid, or account doesn't exist
-            return await Task.FromResult<ClaimsIdentity>(null);
+            return new OkObjectResult("Account created");
         }
     }
 }
